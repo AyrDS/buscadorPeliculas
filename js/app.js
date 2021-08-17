da636ce81c318b09aa264bc4c97163c2dcaf68f0
AyrDS <ayrtonds97@gmail.com>
2021-08-17 04:08:19 -0300

    Solicitud a API terminada

--- js/app.js
+++ js/app.js
@@ -1,18 +1,68 @@
 const formulario = document.querySelector("#formulario");
+const contenedorBuscador = document.querySelector("#contenedorBuscador");
 
 document.addEventListener("DOMContentLoaded", () => {
-    formulario.addEventListener("submit", prueba)
+    formulario.addEventListener("submit", validarFormulario)
 });
 
-function prueba(e){
+function validarFormulario(e) {
     e.preventDefault();
-    const busqueda = document.querySelector("#busqueda").value;
-    console.log("aprestaste buscar")
+    const nombre = document.querySelector("#nombre").value;
+    const year = document.querySelector("#year").value;
 
+    if (nombre === "") {
+        mostrarMensaje("El título de la película es obligatorio");
+
+        return;
+    }
+
+    consultarAPI(nombre, year);
+}
+
+async function consultarAPI(nombre, year) {
     const key = "b42adafc";
-    const url = `http://www.omdbapi.com/?t=${busqueda}&apikey=${key}`;
+    const url = `http://www.omdbapi.com/?t=${nombre}&y=${year}&apikey=${key}`;
+    try {
+        const respuesta = await fetch(url);
+        const resultado = await respuesta.json();
+        console.log(resultado)
+        mostrarPelicula(resultado);
+    } catch (error) {
+        console.log(error);
+        mostrarMensaje("Error de conexión")
+    }
+}
 
-    fetch(url)
-        .then(respuesta => respuesta.json())
-        .then(resultado => console.log(resultado))
+function mostrarMensaje(mensaje) {
+    const existeAlerta = document.querySelector(".error");
+
+    if (!existeAlerta) {
+        const divMensaje = document.createElement("div");
+        divMensaje.classList.add("alert", "alert-danger", "text-center", "mt-3", "mt-lg-0", "fw-bold", "error");
+        divMensaje.textContent = mensaje;
+
+        contenedorBuscador.appendChild(divMensaje);
+
+        setTimeout(() => {
+            divMensaje.remove();
+        }, 5000);
+    };
+}
+
+function limpiarHTML() {
+    while (contenedorBuscador.firstChild) {
+        contenedorBuscador.removeChild(contenedorBuscador.firstChild);
+    };
+}
+
+function mostrarPelicula(pelicula) {
+    limpiarHTML();
+
+    if (pelicula.Response) {
+        pelicula.forEach(peli => {
+            const { Title, Director, Genre, Poster, Released, Runtime, Actors, Year, imdbRating } = peli;
+        });
+    } else {
+        mostrarMensaje("Película no encontrada");
+    }
 }
